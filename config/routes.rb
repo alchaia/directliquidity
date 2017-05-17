88e6cc68821c8528e075e3b6c7f875d43f80d070
alchaia <alex.chaia@directfx.com>
2017-05-17 22:07:52 +0100

    portuguese

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/homepage'
   root 'welcome#homepage'
 
+  get 'welcome/portuguese', to: 'welcome#portuguese'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
