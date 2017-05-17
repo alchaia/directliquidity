9c836c2396e5014fa3dcd42c17a3589ede643475
alchaia <alex.chaia@directfx.com>
2017-05-18 00:51:16 +0100

    languages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   root 'welcome#homepage'
 
   get 'welcome/portuguese', to: 'welcome#portuguese'
+  get 'welcome/german', to: 'welcome#german'
+  get 'welcome/spanish', to: 'welcome#spanish'
+  get 'welcome/italian', to: 'welcome#italian'
+  get 'welcome/french', to: 'welcome#french'
+  get 'welcome/russian', to: 'welcome#russian'
+  get 'welcome/chinese', to: 'welcome#chinese'
+  get 'welcome/arabic', to: 'welcome#arabic'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
